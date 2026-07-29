504d54662ed95918ccb2d598dcae5c52e9df6ab5
Hygor Lorençato <hygorlc92@gmail.com>
2026-07-29 17:43:09 -0300

    Reveal catalog video after YouTube overlay fades

--- src/components/course/HeroCarousel.tsx
+++ src/components/course/HeroCarousel.tsx
@@ -4,7 +4,7 @@ export function HeroVideo() {
   return (
     <div className="relative aspect-video w-full overflow-hidden bg-black md:aspect-[21/9]">
       <iframe
-        className="pointer-events-none absolute left-1/2 top-1/2 aspect-video h-auto w-full -translate-x-1/2 -translate-y-1/2"
+        className="hero-video-reveal pointer-events-none absolute left-1/2 top-1/2 aspect-video h-auto w-full -translate-x-1/2 -translate-y-1/2"
         src={`https://www.youtube-nocookie.com/embed/${VIDEO_ID}?autoplay=1&mute=1&loop=1&playlist=${VIDEO_ID}&controls=0&disablekb=1&fs=0&iv_load_policy=3&rel=0&modestbranding=1&playsinline=1`}
         title="Vídeo em destaque"
         allow="autoplay; encrypted-media"

de017a0c9098c770aea74d14cc6b1b63ace9e1d6
Hygor Lorençato <hygorlc92@gmail.com>
2026-07-29 16:39:18 -0300

    Reduce featured catalog video size

--- src/components/course/HeroCarousel.tsx
+++ src/components/course/HeroCarousel.tsx
@@ -2,7 +2,7 @@ const VIDEO_ID = "RLBZNpJHjpI";
 
 export function HeroVideo() {
   return (
-    <div className="relative aspect-video w-full overflow-hidden bg-black">
+    <div className="relative mx-auto aspect-video w-full overflow-hidden bg-black lg:w-[92%]">
       <iframe
         className="absolute inset-0 h-full w-full"
         src={`https://www.youtube-nocookie.com/embed/${VIDEO_ID}?autoplay=1&mute=1&loop=1&playlist=${VIDEO_ID}&controls=0&rel=0&modestbranding=1&playsinline=1`}

d119f6f0d2aee584bff93384bae0a894a018efda
Hygor Lorençato <hygorlc92@gmail.com>
2026-07-30 10:52:15 -0300

    Reduce catalog video height further

--- src/components/course/HeroCarousel.tsx
+++ src/components/course/HeroCarousel.tsx
@@ -2,7 +2,7 @@ const VIDEO_ID = "RLBZNpJHjpI";
 
 export function HeroVideo() {
   return (
-    <div className="relative aspect-video w-full overflow-hidden bg-black md:aspect-[21/9]">
+    <div className="relative aspect-video w-full overflow-hidden bg-black md:aspect-[5/2]">
       <iframe
         className="hero-video-reveal pointer-events-none absolute left-1/2 top-1/2 aspect-video h-auto w-full -translate-x-1/2 -translate-y-1/2"
         src={`https://www.youtube-nocookie.com/embed/${VIDEO_ID}?autoplay=1&mute=1&loop=1&playlist=${VIDEO_ID}&controls=0&disablekb=1&fs=0&iv_load_policy=3&rel=0&modestbranding=1&playsinline=1`}
